9fcda7c906371cd4fadbb66c1f9547bee754c3a2
n38757902 <n38757902@gmail.com>
2024-08-28 21:26:44 +0300

    Initional commit

---
+++ src/main/java/org/example/conditional/ProductionProfile.java
@@ -0,0 +1,9 @@
+package org.example.conditional;
+
+public class ProductionProfile implements SystemProfile {
+    @Override
+    public String getProfile() {
+        return "Current profile is production";
+    }
+}
+
---
+++ src/main/java/org/example/conditional/SystemProfile.java
@@ -0,0 +1,6 @@
+package org.example.conditional;
+
+public interface SystemProfile {
+    String getProfile();
+}
+
